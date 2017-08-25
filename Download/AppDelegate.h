b2d0c618b53be17b31d3e9a5474bd9d312ec5132
herody <314622981@qq.com>
2017-08-25 10:29:49 +0800

    Initial Commit

---
+++ Download/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Download
+//
+//  Created by 侯亚迪 on 17/8/25.
+//  Copyright © 2017年 杭州魔品科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Download/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Download
+//
+//  Created by 侯亚迪 on 17/8/25.
+//  Copyright © 2017年 杭州魔品科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
